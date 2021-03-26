1b5d11f0bf4efd84174bf19ec0cd6094a2d1dd4e
javivc99 <81316300+javivc99@users.noreply.github.com>
2021-03-26 11:17:43 +0100

    w

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ var audioterminado = new Audio('src/terminado.mp3');
 var repeticion = 1;
 var div = document.getElementById("reps");
 var exname = document.getElementById('exname')
+var textodescanso = document.getElementById('resttime')
 var i = 0;
+var rest = true;
+
+
+let intervalID;
+let contador = 0;
+let tiempolimite = 10;
+
 
 var newsfeed = [
   {
     exercise_name: "Sentadilla con KTB",
     videosrc: "src/sentadilla.mp4",
-    limitrep: 5
+    limitrep: 5,
+    tiempopreparacion: 3,
   },
   {
     exercise_name: "Flexiones mal hechas",
     videosrc: "src/flexion.mp4",
-    limitrep: 6
+    limitrep: 6,
+    tiempopreparacion: 12,
   },
 ];
 
@@ -30,16 +40,35 @@ function carga(fuentevideo) {
 }
 window.onload = carga(newsfeed[i].videosrc)
 
+function playy() {
+    if (rest == true){
+  repeatEverySecond()
+}
+    else {
+    document.getElementById('video-player').play();
+    document.getElementById('video-player').removeAttribute("loop", "false"); 
+      document.getElementById('video-player').removeAttribute("style", "grayscale(100%);");
+    document.getElementById('video-player').setAttribute("autoplay", ""); 
+    }
+  }
+
+
+
 document.querySelector('video').addEventListener('ended', function () {
   if (repeticion == newsfeed[i].limitrep) {
     div.innerHTML ="COMPLETADO!!!";
+    rest = true;
     audioterminado.play();
     i++;
-    carga(newsfeed[i].videosrc);
     repeticion = 1;
     div.innerHTML ="Repeticion " + repeticion + " de " + newsfeed[i].limitrep;
     exname.innerHTML = newsfeed[i].exercise_name;
-  } else {
+    console.log("SIGUIENTE")
+    carga(newsfeed[i].videosrc);
+    contador = 0;
+    playy()
+  }
+  else {
     beep.play();
     this.play();
     ++repeticion;
@@ -47,6 +76,38 @@ document.querySelector('video').addEventListener('ended', function () {
     exname.innerHTML = newsfeed[i].exercise_name;
   }
 })
+
+
+function repeatEverySecond() {
+  intervalID = setInterval(sendMessage, 1000);
+      document.getElementById('video-player').style.filter = "grayscale(100%)"
+    document.getElementById('video-player').play();
+    textodescanso.innerHTML ="Preparate, empezamos en <br><br>" + (newsfeed[i].tiempopreparacion-contador);
+  
+}
+
+function sendMessage(){
+    if (contador !== newsfeed[i].tiempopreparacion){
+    document.getElementById('video-player').setAttribute("loop", "true"); 
+
+    contador++
+    textodescanso.innerHTML ="Preparate, empezamos en <br><br>" + (newsfeed[i].tiempopreparacion-contador);
+}
+else {
+rest = false
+playy()
+clearAlert()
+    textodescanso.innerHTML ="";
+}
+}
+
+function clearAlert() {
+  clearTimeout(intervalID);
+}
+
+
+
+
 
 
 
